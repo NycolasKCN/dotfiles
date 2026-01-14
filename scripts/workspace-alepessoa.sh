99ce9f232f26ba0f4a352057159221c367daa9bb
Nycolas Kevin <nycolas.kevin2@gmail.com>
2026-01-14 14:35:36 -0300

    keybinds, rules, new script, alias

--- scripts/workspace-alepessoa.sh
+++ scripts/workspace-alepessoa.sh
@@ -3,7 +3,8 @@
 notify-send -u low "Workspaces" "Starting 'Ale pessoa' dev workspace."
 
 docker start ale-pessoa-db ale-pessoa-minio
-hyprctl dispatch exec "[workspace 2 silent]" webstorm ~/projects/ayty/alePessoa/front-end
-hyprctl dispatch exec "[workspace 3 silent]" idea1 ~/projects/ayty/alePessoa/back-end
+
+hyprctl dispatch exec "[monitor DP-1;workspace 2 silent]" bash -sh "webstorm ~/projects/ayty/alePessoa/front-end &"
+hyprctl dispatch exec "[monitor DP-1;workspace 3 silent]" bash -sh "idea1 ~/projects/ayty/alePessoa/back-end &"
 
 notify-send -u low "Workspaces" "Open 'Ale pessoa' dev workspace concluded."
