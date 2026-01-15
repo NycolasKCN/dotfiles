1051b8322b34619ef4f35276c258bfc438005c45
Nycolas Kevin <nycolas.kevin2@gmail.com>
2026-01-15 11:11:33 -0300

    workspace-alepessoa

--- scripts/workspace-alepessoa.sh
+++ scripts/workspace-alepessoa.sh
@@ -4,7 +4,7 @@ notify-send -u low "Workspaces" "Starting 'Ale pessoa' dev workspace."
 
 docker start ale-pessoa-db ale-pessoa-minio
 
-hyprctl dispatch exec "[monitor DP-1;workspace 2 silent]" bash -sh "webstorm ~/projects/ayty/alePessoa/front-end &"
-hyprctl dispatch exec "[monitor DP-1;workspace 3 silent]" bash -sh "idea1 ~/projects/ayty/alePessoa/back-end &"
+hyprctl dispatch exec "[monitor DP-1;workspace 2 silent]" ~/.local/bin/webstorm ~/projects/ayty/alePessoa/front-end
+hyprctl dispatch exec "[monitor DP-1;workspace 3 silent]" ~/.local/bin/idea1 ~/projects/ayty/alePessoa/back-end
 
 notify-send -u low "Workspaces" "Open 'Ale pessoa' dev workspace concluded."
